b126fd51fd360d31901ec58d04e34fcc8bd0f060
lmAshishMishra <158290112+lmAshishMishra@users.noreply.github.com>
2026-01-10 13:32:20 +0530

    Added new migration to add foreign key constraint n one to many relation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-
+const {CityController} =require('./controllers');
 const {ServerConfig,Logger} = require('./config')
 const app=express();
 const apiRoutes=require('./routes');
@@ -7,7 +7,19 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 console.log('home');
 app.use('/api',apiRoutes);
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async ()=>{
 console.log(`Successfully start project: ${ServerConfig.PORT}`)
 Logger.info("Successfully start server","root",{})
+const {City,Airport} =require('./models');
+
+await City.destroy({
+    where:{
+        id:9
+    }
+
+});
+
+// const city =await City.findByPk(9);
+// await city.createAirport({name:'Indore Airport',code:'IND'});
+
 });
