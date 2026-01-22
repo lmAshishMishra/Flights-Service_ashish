ea91cfb09ac2b53a00b35c8b30335b699a27bbba
lmAshishMishra <158290112+lmAshishMishra@users.noreply.github.com>
2026-01-22 08:59:51 +0530

    Added seeds

--- src/models/seat.js
+++ src/models/seat.js
@@ -11,10 +11,14 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+     static associate(models) {
       // define association here
+      this.belongsTo(models.Airplane, {
+        foreignKey: 'airplaneId',
+      });
     }
   }
+  
   Seat.init({
   airplaneId: {
       type: DataTypes.INTEGER,
